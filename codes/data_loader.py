6b67832ff2c0b19bdd57e607e8a2e0c6b006fdc9
Quang <quangnb2000@gmail.com>
2021-04-22 01:54:14 +0700

    Sua data_loader.py cho SCADA

--- codes/data_loader.py
+++ codes/data_loader.py
@@ -72,82 +72,25 @@ class DataGenerator(BaseDataGenerator):
                 self.train_set_lstm = dict(data=all_train_set_lstm)
                 self.val_set_lstm = dict(data=all_val_set_lstm)
 
-        elif 'scada' in dataset:
-            all_train_set_vae = []
-            all_val_set_vae = []
-            all_test_set_vae = []
-            all_train_set_lstm = []
-            all_val_set_lstm = []
-            for j in range(1, 5):
-                print("SCADA")
-                data_dir = '../datasets/NAB-known-anomaly/{}/'.format(dataset)
-                data = np.load(data_dir + dataset + '_{}.npz'.format(j))
-
-                # normalise the dataset by training set mean and std
-                train_m = data['train_m']
-                train_std = data['train_std']
-                # readings_normalised = (data['readings'] - train_m) / train_std
-
-                # slice training set into rolling windows
-                n_train_sample = len(data['training'])
-                n_train_vae = n_train_sample - self.config['l_win'] + 1
-                # rolling_windows = np.zeros((n_train_vae, self.config['l_win'], self.config['n_channel'])) #them n_channel dong nay
-                # for i in range(n_train_sample - self.config['l_win'] + 1):
-                #     rolling_windows[i] = np.reshape(data['training'][i:i + self.config['l_win']],(self.config['l_win'], self.config['n_channel']))
-                stride_ori = data['training'].reshape((-1, self.config['n_channel'])).strides
-                strides = np.insert(stride_ori, 0, stride_ori[0], axis = 0)
-                shape = [n_train_vae, self.config['l_win'], self.config['n_channel']]
-
-                rolling_windows = np.lib.stride_tricks.as_strided(data['training'],shape, strides, writeable = False)
-
-                # create VAE training and validation set
-                idx_train, idx_val, self.n_train_vae, self.n_val_vae = self.separate_train_and_val_set(n_train_vae)
-                all_train_set_vae.extend(rolling_windows[idx_train])
-                all_val_set_vae.extend(rolling_windows[idx_val])
-                all_test_set_vae.extend(rolling_windows[idx_val[:self.config['batch_size']]])
-
-                # create LSTM training and validation set
-                for k in range(self.config['l_win']):
-                    n_not_overlap_wins = (n_train_sample - k) // self.config['l_win']
-                    n_train_lstm = n_not_overlap_wins - self.config['l_seq'] + 1
-                    shape = [n_train_lstm, self.config['l_seq'], self.config['l_win'], self.config['n_channel']]
-                    strides = np.insert(stride_ori, 0, [stride_ori[0]*self.config['l_win'], stride_ori[0]*self.config['l_win']], axis = 0)
-                    cur_lstm_seq = np.lib.stride_tricks.as_strided(data['training'][k:],shape, strides, writeable = False)
-                    if k == 0:
-                        lstm_seq = cur_lstm_seq
-                    else:
-                        lstm_seq = np.concatenate((lstm_seq, cur_lstm_seq), axis=0)
-                n_train_lstm = lstm_seq.shape[0]
-                idx_train, idx_val, self.n_train_lstm, self.n_val_lstm = self.separate_train_and_val_set(n_train_lstm)
-                all_train_set_lstm.extend(lstm_seq[idx_train])
-                all_val_set_lstm.extend(lstm_seq[idx_val])
-            # create VAE training and validation set
-            self.train_set_vae = dict(data=np.reshape(all_train_set_vae, (-1, self.config['l_win'], self.config['n_channel'])))
-            self.val_set_vae = dict(data=np.reshape(all_val_set_vae, (-1, self.config['l_win'], self.config['n_channel'])))
-            self.test_set_vae = dict(data=np.reshape(all_test_set_vae, (-1, self.config['l_win'], self.config['n_channel'])))
-            # create LSTM training and validation set
-            if self.config['n_channel'] == 1: #them dong nay
-                self.train_set_lstm = dict(data=np.expand_dims(all_train_set_lstm, -1))
-                self.val_set_lstm = dict(data=np.expand_dims(all_val_set_lstm, -1))
-            else:
-                self.train_set_lstm = dict(data=all_train_set_lstm)
-                self.val_set_lstm = dict(data=all_val_set_lstm)
-
         else:
             data_dir = '../datasets/NAB-known-anomaly/'
             data = np.load(data_dir + dataset + '.npz')
 
             # normalise the dataset by training set mean and std
-            train_m = data['train_m']
-            train_std = data['train_std']
-            readings_normalised = (data['readings'] - train_m) / train_std
+            # train_m = data['train_m']
+            # train_std = data['train_std']
+            # readings_normalised = (data['readings'] - train_m) / train_std
 
             # slice training set into rolling windows
             n_train_sample = len(data['training'])
+            stride_ori = data['training'].reshape((-1,self.config['n_channel'])).strides
+            strides = np.insert(stride_ori, 0, stride_ori[0], axis = 0)
             n_train_vae = n_train_sample - self.config['l_win'] + 1
-            rolling_windows = np.zeros((n_train_vae, self.config['l_win'], self.config['n_channel'])) #them n_channel dong nay
-            for i in range(n_train_sample - self.config['l_win'] + 1):
-                rolling_windows[i] = np.reshape(data['training'][i:i + self.config['l_win']],(self.config['l_win'], self.config['n_channel']))
+            shape = [n_train_vae, self.config['l_win'], self.config['n_channel']]
+            rolling_windows = np.lib.stride_tricks.as_strided(data['training'],shape, strides, writeable = False)
+            # rolling_windows = np.zeros((n_train_vae, self.config['l_win'], self.config['n_channel'])) #them n_channel dong nay
+            # for i in range(n_train_sample - self.config['l_win'] + 1):
+            #     rolling_windows[i] = np.reshape(data['training'][i:i + self.config['l_win']],(self.config['l_win'], self.config['n_channel']))
 
             # create VAE training and validation set
             idx_train, idx_val, self.n_train_vae, self.n_val_vae = self.separate_train_and_val_set(n_train_vae)
@@ -159,14 +102,10 @@ class DataGenerator(BaseDataGenerator):
             for k in range(self.config['l_win']):
                 n_not_overlap_wins = (n_train_sample - k) // self.config['l_win']
                 n_train_lstm = n_not_overlap_wins - self.config['l_seq'] + 1
+                shape = [n_train_lstm, self.config['l_seq'], self.config['l_win'], self.config['n_channel']]
+                strides = np.insert(stride_ori, 0, [stride_ori[0]*self.config['l_win'], stride_ori[0]*self.config['l_win']], axis = 0)
+                cur_lstm_seq = np.lib.stride_tricks.as_strided(data['training'][k:],shape, strides, writeable = False)
                 cur_lstm_seq = np.zeros((n_train_lstm, self.config['l_seq'], self.config['l_win'], self.config['n_channel']))
-                for i in range(n_train_lstm):
-                    cur_seq = np.zeros((self.config['l_seq'], self.config['l_win'], self.config['n_channel']))
-                    for j in range(self.config['l_seq']):
-                        # print(k,i,j)
-                        cur_seq[j] = np.reshape(data['training'][k + self.config['l_win'] * (j + i): k + self.config['l_win'] * (j + i + 1)]\
-                                                ,(self.config['l_win'], self.config['n_channel']))
-                    cur_lstm_seq[i] = cur_seq
                 if k == 0:
                     lstm_seq = cur_lstm_seq
                 else:
