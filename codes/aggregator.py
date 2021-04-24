e0ddde2b3ecc47be99953d7e024e52d473a0ba74
Le Quang <quangnb2000@gmail.com>
2021-04-24 10:29:54 +0700

    Ignore weights files

--- codes/aggregator.py
+++ codes/aggregator.py
@@ -34,9 +34,9 @@ class Aggregator:
                     for i in range(len(self.vae_trainers[0].model.train_vars_VAE)):
                         global_train_var_eval = np.zeros_like(self.vae_trainers[0].model.train_vars_VAE[i].eval(self.vae_trainers[0].sess))
                         for j in range(len(self.vae_trainers)):
-                            if comm_round == 1:
-                                pickle.dump(self.vae_trainers[j].model.train_vars_VAE[i].eval(self.vae_trainers[j].sess), "Weights.h5")
-                                exit()
+                            if comm_round == 1 and j == 0:
+                                pickle.dump(self.vae_trainers[j].model.train_vars_VAE[i].eval(self.vae_trainers[j].sess), open( "Weights_{}.h5".format(i), "wb" ))
+                                print("VAE_{}".format(i))
                             else:
                                 global_train_var_eval += np.multiply(self.weights[j], self.vae_trainers[j].model.train_vars_VAE[i].eval(self.vae_trainers[j].sess))
                         self.global_vae_trainer.model.train_vars_VAE[i].load(global_train_var_eval, self.global_vae_trainer.sess)
